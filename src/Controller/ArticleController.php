533994745696317592c25a9711bc8e06ef67717f
maxc0434 <max.couillet@gmail.com>
2025-09-03 16:42:41 +0200

    maj notation presque ok, affichage comment pas ok

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,7 @@ use App\Form\ArticleType;
 use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use App\Repository\CategorieRepository;
+use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +27,24 @@ final class ArticleController extends AbstractController
             'categories' => $categorieRepository->findAll(),
         ]);
     }
+
+    #[Route('/articles/{id}', name: 'app_article_filter', methods: ['GET'])]
+    public function filter(ArticleRepository $articleRepository, CategorieRepository $categorieRepository, ?int $id = null): Response
+    {
+        if ($id) {
+            $articles = $articleRepository->findBy(['categorie' => $id]);
+        } else {
+            $articles = $articleRepository->findAll();
+        }
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles,
+            'categories' => $categorieRepository->findAll(),
+        ]);
+    }
+
+
+
 
     #[Route('/new', name: 'app_article_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
@@ -55,32 +74,32 @@ final class ArticleController extends AbstractController
     //     ]);
     // }
 
-    #[Route('/{id}', name: 'app_article_show', methods: ['GET','POST'])]
-public function show(Article $article, Request $request, EntityManagerInterface $em): Response
-{
-    $comment = new Comment();
-    $comment->setArticle($article);
-    $comment->setAutor($this->getUser());
-    $comment->setDate(new \DateTime());
-    $form = $this->createForm(CommentType::class, $comment);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        // $comment->setAutor($this->getUser());
-        // $comment->setContent($form->get('content')->getData());
-        $em->persist($comment);
-        $em->flush();
+    #[Route('/{id}', name: 'app_article_show', methods: ['GET', 'POST'])]
+    public function show(CommentRepository $commentRepository, Article $article, Request $request, EntityManagerInterface $em): Response
+    {
         
-        return $this->redirectToRoute('app_article_index');
+        $comment = new Comment();
+        $comment->setArticle($article);
+        $comment->setAutor($this->getUser());
+        $comment->setDate(new \DateTime());
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($comment);
+            $em->flush();
+            
+            return $this->redirectToRoute('app_article_index');
+        }
+        
+        // $comments = $commentRepository->findBy(['article' => $article]);
+       
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+            'comments' => $comment,
+        ]);
     }
-
-    return $this->render('article/show.html.twig', [
-        'article' => $article,
-        'form' => $form->createView(),
-    ]);
-}
-
-
 
     #[Route('/{id}/edit', name: 'app_article_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Article $article, EntityManagerInterface $entityManager): Response
@@ -103,11 +122,12 @@ public function show(Article $article, Request $request, EntityManagerInterface 
     #[Route('/{id}', name: 'app_article_delete', methods: ['POST'])]
     public function delete(Request $request, Article $article, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($article);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('app_article_index', [], Response::HTTP_SEE_OTHER);
     }
+
 }
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -2,13 +2,15 @@
 
 namespace App\Form;
 
-use App\Entity\Comment;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class CommentType extends AbstractType
 {
@@ -17,10 +19,20 @@ class CommentType extends AbstractType
          $builder
             ->add('content')
             ->add('date')
-            ->add('note', IntegerType::class, [
-                'label' => 'Note',
-                'attr' => ['min' => 1, 'max' => 5],
-            ])
+            ->add('note', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+    'expanded' => true,    // rend des radios au lieu d’un select
+    'multiple' => false,
+    'label' => 'Note',
+    'required' => true,
+])
+
             ->add('autor', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'nom',
